418a182133fa9ea956fd296f6eadf0ebe3a5695c
BSS08 <birtejsoin@gmail.com>
2026-03-15 21:43:55 +0530

    UC3:Palindrome String Reverse

--- src/PALINDROME.java
+++ src/PALINDROME.java
@@ -1,18 +1,27 @@
 import java.util.Scanner;
+
 class PalindromeCheckerApp{
-    public static void main(String[]args){
-        Scanner sc=new Scanner(System.in);
-        System.out.print("Enter a string:");
-        String str=sc.nextLine();
-        String reversed="";
-        for(int i=str.length()-1;i>=0;i--){
-            reversed=reversed+str.charAt(i);
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a string: ");
+        String str = sc.nextLine();
+
+        String reversed = "";
+
+        for(int i = str.length() - 1; i >= 0; i--) {
+            reversed = reversed + str.charAt(i);
         }
-        if(str.equals(reversed)){
+
+        if(str.equals(reversed)) {
             System.out.println("The string is a Palindrome.");
-        }else{
+        }
+        else {
             System.out.println("The string is NOT a Palindrome.");
         }
+
         sc.close();
     }
 }
