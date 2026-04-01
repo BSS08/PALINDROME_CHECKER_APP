b584437a0b0813c7bde099fee0c5adb353a3f61b
BSS08 <birtejsoin@gmail.com>
2026-04-01 12:22:19 +0530

    Case-Insensitive Palindrome

--- src/PALINDROME.java
+++ src/PALINDROME.java
@@ -2,16 +2,6 @@ import java.util.Scanner;
 
 public class PALINDROME {
 
-    static boolean isPalindrome(String str, int start, int end) {
-        if (start >= end) {
-            return true;
-        }
-        if (str.charAt(start) != str.charAt(end)) {
-            return false;
-        }
-        return isPalindrome(str, start + 1, end - 1);
-    }
-
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
@@ -19,12 +9,26 @@ public class PALINDROME {
         System.out.print("Enter a string: ");
         String input = scanner.nextLine();
 
-        boolean result = isPalindrome(input, 0, input.length() - 1);
+        String normalized = input.replaceAll("\\s+", "").toLowerCase();
+
+        int start = 0;
+        int end = normalized.length() - 1;
+        boolean isPalindrome = true;
+
+        while (start < end) {
+            if (normalized.charAt(start) != normalized.charAt(end)) {
+                isPalindrome = false;
+                break;
+            }
+            start++;
+            end--;
+        }
 
         System.out.println("Input: " + input);
-        System.out.println("Is Palindrome? : " + result);
+        System.out.println("Is Palindrome? : " + isPalindrome);
 
         scanner.close();
     }
 }
+
 
